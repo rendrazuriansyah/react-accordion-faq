acd9dfa4519510f920e1368e48ff0e350f4a7981
rendrazuriansyah <rendra.zurd04@gmail.com>
2024-08-21 12:33:25 +0700

    Add toggle functionality to AccordionItem component Implement useState to manage open/close state Add handleToggle function to toggle state on click Update AccordionItem component to conditionally render content based on state Adjust number formatting for single-digit numbers

--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 export default function AccordionItem({ number, question, answer }) {
+	const [isOpen, setIsOpen] = useState(false);
+
+	function handleToggle() {
+		setIsOpen(!isOpen);
+	}
+
 	return (
-		<div className="item">
-			<p className="number">{number}</p>
+		<div
+			className={`item ${isOpen ? "open" : ""}`}
+			onClick={handleToggle}
+		>
+			<p className="number">{number < 10 ? `0${number}` : number}</p>
 			<p className="title">{question}</p>
-			<p className="icon">+</p>
-			<div className="content-box">{answer}</div>
+			<p className="icon">{isOpen ? "-" : "+"}</p>
+			{isOpen && <div className="content-box">{answer}</div>}
 		</div>
 	);
 }
